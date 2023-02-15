f2196e75a3a95491a047ce044186e2644973b414
Alex Kochutov <alex.kochutov@gmail.com>
2023-02-15 17:24:48 +0400

    feat: Add Cypress ProjectID for linking to Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'zowtvf',
   e2e: {
     baseUrl: "http://qamid.tmweb.ru",
     setupNodeEvents(on, config) {
